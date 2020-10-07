c673a650aaa130e0252d2cc67f477a78bd67e22e
berbersky <60584511+berbersky@users.noreply.github.com>
2020-10-07 11:18:37 +0200

    initial commit

--- src/Client.jsx
+++ src/Client.jsx
@@ -1,14 +1,15 @@
-import React, { Component } from "react";
+/**
+ * Functional component Client
+ * @param {,Object }  details,
+ * @param {,Function} onDelete
+ * @returns {,<Client/>}
+ */
+const Client = ({ details, onDelete }) => (
+  <li>
+    {details.name}
+    <button onClick={() => onDelete(details.id)}>X</button>
+  </li>
+);
 
-export default class Client extends Component {
-  render() {
-    return (
-      <li>
-        {this.props.details.name}{" "}
-        <button onClick={() => this.props.onDelete(this.props.details.id)}>
-          X
-        </button>
-      </li>
-    );
-  }
-}
+export default Client;
+
